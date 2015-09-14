b958fcd920d2508c48bad803222ce6d56f8dcdfb
Blaxon <asasdyy@163.com>
2015-09-14 10:22:35 +0800

    Correct file's name again

---
+++ AI2048.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+The minigame 2048 in python
+
+this is the ai for 2048
+
+"""
+
+import game2048
+
+if __name__ == '__main__':
+    game2048.play()
+
+
+
+
+
+
